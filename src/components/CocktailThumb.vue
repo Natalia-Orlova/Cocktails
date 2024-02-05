bd8dd37e16087154305825ca16e0c22aaaf43384
NataliaOrlova <natakarelskaya@yandex.ru>
2024-02-05 21:51:27 +0400

    cocktails

---
+++ src/components/CocktailThumb.vue
@@ -0,0 +1,52 @@
+<script setup>
+defineProps({
+  cocktail: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="root">
+    <router-link :to="`/cocktails/${cocktail.idDrink}`">
+      <div
+        :style="`background-image: url(${cocktail.strDrinkThumb})`"
+        class="pic"
+      ></div>
+    </router-link>
+    <div class="name">{{ cocktail.strDrink }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/styles/main";
+
+.root {
+  width: 120px;
+  margin-right: 38px;
+  padding-bottom: 20px;
+
+  &:nth-child(3n + 3) {
+    margin-right: 0;
+  }
+}
+
+.pic {
+  width: 120px;
+  height: 120px;
+  margin: 0 auto;
+  background-repeat: no-repeat;
+  background-position: 50% 50%;
+  background-size: 100%;
+}
+
+.name {
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin-top: 15px;
+  letter-spacing: 0.1em;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { ROUTES_PATHS } from '@/constants'
+import HomePage from '../pages/HomePage.vue'
+import CocktailPage from '../pages/CocktailPage.vue'
+import RandomCocktail from '../pages/RandomCocktail.vue'
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: ROUTES_PATHS.HOME,
+      name: ROUTES_PATHS.HOME,
+      component: HomePage
+    },
+    {
+      path: ROUTES_PATHS.COCKTAIL,
+      name: ROUTES_PATHS.COCKTAIL,
+      component: CocktailPage
+    },
+    {
+      path: ROUTES_PATHS.RANDOM_COCKTAIL,
+      name: ROUTES_PATHS.RANDOM_COCKTAIL,
+      component: RandomCocktail
+    },
+    
+  ]
+})
+
+export default router
